d8515eb526e323fdc62b092471e61c10017ab6ff
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-09-06 11:44:21 +0800

    daftar & login pengguna

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -16,15 +16,26 @@ export class Tab1Page {
   //tab1.page.ts
   email: any;
   password: any;
+  message:any;
   registerUser() {
         return new Promise<any>((resolve, reject) => {
-     
           this.afAuth.createUserWithEmailAndPassword(this.email, this.password)
             .then(
               res => resolve(res),
               err => reject(err))
         })
-      }
-    
-
+      }//end register
+    cubaregister() {
+          this.registerUser()
+            .then(res => {
+              console.log(res);
+              this.message= "Berjaya daftar";
+              alert("Berjaya daftar");
+              //this.navCtrl.navigateForward('/dashboard');
+            }, err => {
+              this.message= err.message;
+              alert("Tidak berjaya daftar");
+            })
+      //alert (this.message);
+    }//end cubalogin
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
+//tab2.page.ts
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  email:any;
+  password:any;
+  message:any;
 
-  constructor() {}
+  constructor(private afAuth: AngularFireAuth) {}
+
+  cubalogin() {
+        this.login()
+          .then(res => {
+            console.log(res);
+            this.message= "Berjaya login";
+            alert("Berjaya login");
+            //this.navCtrl.navigateForward('/dashboard');
+          }, err => {
+            this.message= err.message;
+            alert("Tidak berjaya login");
+          })
+    //alert (this.message);
+  }//end cubalogin
+   
+    
+  //tambah fungsi
+  login() {
+        return new Promise<any>((resolve, reject) => {
+      //authenticating with email & password
+          this.afAuth.signInWithEmailAndPassword(this.email, this.password)
+            .then(
+              res => resolve(res),  //successful
+              err => resolve(err)
+        )  //rejected
+        })
+  }//end cubalogin
+    
 
 }
