e374e2b1132b27159741e62368b39b76088864ab
Khirulnizam Abd Rahman <khirulnizam@gmail.com>
2021-09-13 17:30:32 +0800

    Listing data from firebase

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
+//tab3.page.ts
 import { Component } from '@angular/core';
-
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -7,6 +8,30 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(
+    private firestore: AngularFirestore) {}
+
+    //activities:any;
+  activities = [];//hold records
+  ngOnInit() {
+    //read activities from firestore
+    this.readActivities().subscribe(data => {
+
+      this.activities = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          isEdit: false,
+          namaaktiviti: e.payload.doc.data()['namaaktiviti'],
+          tarikhaktiviti: e.payload.doc.data()['tarikhaktiviti'],
+        };
+      })
+      console.log(this.activities);
+    });//end data.map 
+  }//end onInit
+
+    readActivities() {
+      //fetch all records
+        return this.firestore.collection('aktivitimasjid').snapshotChanges();
+    }
 
 }
